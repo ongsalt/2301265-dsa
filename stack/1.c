a26d75dc5aa34cbd2bb6d9fb216d148a9316d068
Ongsalt <ongsa.srp.mkm@gmail.com>
2025-08-19 10:07:23 +0700

    stack in zig becuase why not

---
+++ stack/1.c
@@ -0,0 +1,47 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  size_t capacity;
+  size_t size;
+  char *items;
+} Stack＜char＞;
+
+typedef struct {
+  bool is_some;
+  char value; // this is pain
+} Option＜char＞;
+
+Stack＜char＞ new(size_t initial_capacity) {
+  Stack＜char＞ stack = {
+      .capacity = 128, .size = 0, .items = malloc(sizeof(char) * 128)};
+  return stack;
+}
+
+void push(Stack＜char＞ *stack, char c) {
+  if (stack->capacity == stack->size) {
+    stack->capacity *= 2;
+    realloc(stack->items, stack->capacity);
+  }
+  stack->items[stack->size] = c;
+  stack->size += 1;
+}
+
+Option＜char＞ pop(Stack＜char＞ *stack) {
+  Option＜char＞ o = {0};
+  if (stack->size != 0) {
+    stack->size -= 1;
+    o.value = stack->items[stack->size];
+    o.is_some = true;
+  }
+  return o;
+}
+
+int main() {
+  char input[] =
+      "let mut b = 8; for a in 1..10 { println!(\"Hello {a} {b}\"); b += a; }";
+
+  char *cursor = input;
+}
---
+++ stack/stack.zig
@@ -0,0 +1,64 @@
+const std = @import("std");
+
+pub fn Stack(comptime T: type) type {
+    return struct {
+        allocator: std.mem.Allocator,
+        capacity: usize,
+        size: usize,
+        items: []T,
+
+        const Self = @This();
+
+        fn init(allocator: std.mem.Allocator) !Self {
+            const items: []T = try allocator.alloc(T, 128);
+
+            return Self{ .allocator = allocator, .items = items, .capacity = 128, .size = 0 };
+        }
+
+        fn free(self: *Self) void {
+            self.allocator.free(self.items);
+        }
+
+        fn pop(self: *Self) ?T {
+            if (self.size == 0) {
+                return null;
+            }
+            const item = self.items[self.size - 1];
+            self.items[self.size - 1] = undefined;
+            self.size -= 1;
+            return item;
+        }
+
+        fn push(self: *Self, item: T) !void {
+            if (self.size >= self.capacity) {
+                self.capacity *= 2;
+                self.items = self.allocator.realloc(self.items, self.capacity);
+                // no resizing because im too lazy
+            }
+            self.items[self.size] = item;
+            self.size += 1;
+        }
+    };
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
+    const allocator = gpa.allocator();
+
+    var stack = try Stack(u64).init(allocator);
+    defer stack.free();
+    try stack.push(12);
+    try stack.push(24);
+
+    var i = stack.pop();
+    std.debug.print("pop: {?}\n", .{i});
+    std.debug.print("size: {?}\n", .{stack.size});
+
+    i = stack.pop();
+    std.debug.print("pop: {?}\n", .{i});
+    std.debug.print("size: {?}\n", .{stack.size});
+
+    i = stack.pop();
+    std.debug.print("pop: {?}\n", .{i});
+    std.debug.print("size: {?}\n", .{stack.size});
+}
